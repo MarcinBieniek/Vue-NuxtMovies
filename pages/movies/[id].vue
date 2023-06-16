21bbb77213f29890f7399393be1f23c0f5000b9c
Marcin Bieniek <marcingim@gmail.com>
2023-06-16 14:48:34 +0200

    Add metatags

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -2,12 +2,26 @@
 
   const route = useRoute()
 
-  const { data } = useFetch(
+  const { data } = await useFetch(
     `http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`,
-    { pick: ["Plot", "Title"],
-      key: `/movies/${route.params.id}`
+    { pick: ["Plot", "Title", "Error", "Poster"],
+      key: `/movies/${route.params.id}`,
     }
   );
+
+  if (data.value.Error === "Incorrect IMDb ID.") {
+    showError({ statusCode: 404, statusMessage: "Page Not Found"})
+  }
+
+  useHead({
+    title: data.value.Title,
+    meta: [
+      { name: "description", content: data.value.Plot },
+      { property: "og:description", content: data.value.Plot },
+      { property: "og:image", content: data.value.Poster },
+      { name: "twitter:card", content: `summary_large_image` },
+    ],
+  });
 
 </script>
 
