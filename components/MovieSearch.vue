8fce0fe29740e154d742b7cd75b7986479e85ac3
Marcin Bieniek <marcingim@gmail.com>
2023-06-16 12:38:08 +0200

    Fix data display error

--- components/MovieSearch.vue
+++ components/MovieSearch.vue
@@ -1,12 +1,14 @@
 <script setup>
 
-  const query = ref("")
+  const query = ref("batman")
   const movies = ref([])
 
   async function search(){
     const {Search} = await $fetch(`http://www.omdbapi.com/?apikey=8e3f600b&s=${query.value}`)
     movies.value = Search
   }
+
+  search()
 
 </script>
 
