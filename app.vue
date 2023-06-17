9a41ad5ec9e59ece57fc68b801764da68b51fb7b
Marcin Bieniek <marcingim@gmail.com>
2023-06-17 09:40:53 +0200

    Add abort navigation middleware

--- app.vue
+++ app.vue
@@ -8,11 +8,8 @@
 
 <template>
   <div>
-    <nav style="display: flex; gap: 10px">
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/movies">Movies</NuxtLink>
-      <NuxtLink to="https://www.pzpn.pl">PZPN</NuxtLink>
-    </nav>
-    <NuxtPage/>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav style="display: flex; gap: 10px">
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/movies">Movies</NuxtLink>
+    <NuxtLink to="https://www.pzpn.pl">PZPN</NuxtLink>
+  </nav>
+  <slot></slot>
+</template>
