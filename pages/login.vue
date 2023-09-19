77805ef9009237afc17a56214f04e0f3a670de23
Marcin Bieniek <marcingim@gmail.com>
2023-09-19 22:56:10 +0200

    Add login page

--- pages/login.vue
+++ pages/login.vue
@@ -10,16 +10,18 @@
 </script>
 
 <template>
-  <form @submit.prevent="user.login">
-    <h1>Login</h1>
-    <label>
-      Username
-      <input type="text" />
-    </label>
-    <label>
-      Password
-      <input type="password" />
-    </label>
-    <button>Login</button>
-  </form>
+  <div class="flex h-screen justify-center">
+    <form class="flex flex-col justify-center" @submit.prevent="user.login">
+      <h1 class="text-[30px] font-bold flex justify-center mb-5">Login</h1>
+      <label class="text-[22px] flex justify-center mb-5 items-center">
+        <p class="mr-5">Username</p>
+        <input class="p-[10px]" type="text" />
+      </label>
+      <label class="text-[22px] flex justify-center mb-5 items-center">
+        <p class="mr-5">Password</p>
+        <input class="p-[10px]" type="password" />
+      </label>
+      <button class="bg-blue hover:bg-strongBlue text-white text-[16px] py-[10px] mt-2">Submit</button>
+    </form>
+  </div>
 </template>
